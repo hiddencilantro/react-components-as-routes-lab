8215ba5e018c7242a066c61a5c99e16971a71ba3
hiddencilantro <stevekim1109@gmail.com>
2021-09-30 11:51:14 -0400

    commit tree 181c8d6091aed6afeef0052522a3e00ff6a530a6

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { directors } from '../data';
 
+const createDirectorsList = (directorData) => {
+  return directorData.map(director => {
+    return (
+      <div>
+        <h4>{director.name}</h4>
+        <ul>
+          {director.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  })
+}
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {createDirectorsList(directors)}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { movies } from '../data';
 
+const createMoviesList = (movieData) => {
+  return movieData.map(movie => {
+    return (
+      <div>
+        <h4>{movie.title} / {movie.time} minutes</h4>
+        <ul>
+          {movie.genres.map(genre => <li>{genre}</li>)}
+        </ul>
+      </div>
+    )
+  })
+}
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {createMoviesList(movies)}
     </div>
   );
 };

8331bf3657e84eb97c168820685a7247854fd033
hiddencilantro <stevekim1109@gmail.com>
2021-09-30 11:51:52 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { actors } from '../data';
 
+const createActorsList = (actorData) => {
+  return actorData.map(actor => {
+    return (
+      <div>
+        <h4>{actor.name}</h4>
+        <ul>
+          {actor.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  })
+}
+
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {createActorsList(actors)}
     </div>
   );
 };
